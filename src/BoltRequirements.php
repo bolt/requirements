56bc6e0fdf55f9ce8b395e667481c6955e744d02
Gawain Lynch <gawain.lynch@gmail.com>
2017-09-13 07:12:52 +0200

    Swap out create_function() for anonymous function

--- src/BoltRequirements.php
+++ src/BoltRequirements.php
@@ -187,7 +187,7 @@ final class BoltRequirements extends RequirementCollection
         if (extension_loaded('suhosin')) {
             $this->addPhpConfigRequirement(
                 'suhosin.executor.include.whitelist',
-                create_function('$cfgValue', 'return false !== stripos($cfgValue, "phar");'),
+                function($cfgValue) { return false !== stripos($cfgValue, 'phar'); },
                 false,
                 'suhosin.executor.include.whitelist must be configured correctly in php.ini',
                 'Add "<strong>phar</strong>" to <strong>suhosin.executor.include.whitelist</strong> in php.ini<a href="#phpini">*</a>.'
@@ -201,7 +201,7 @@ final class BoltRequirements extends RequirementCollection
 
             $this->addPhpConfigRecommendation(
                 'xdebug.max_nesting_level',
-                create_function('$cfgValue', 'return $cfgValue > 500;'),
+                function($cfgValue) { return $cfgValue > 500; },
                 true,
                 'xdebug.max_nesting_level should be above 500 in php.ini',
                 'Set "<strong>xdebug.max_nesting_level</strong>" to e.g. "<strong>500</strong>" in php.ini<a href="#phpini">*</a> to stop Xdebug\'s infinite recursion protection erroneously throwing a fatal error in your project.'
@@ -219,7 +219,7 @@ final class BoltRequirements extends RequirementCollection
         if (extension_loaded('mbstring')) {
             $this->addPhpConfigRequirement(
                 'mbstring.func_overload',
-                create_function('$cfgValue', 'return (int) $cfgValue === 0;'),
+                function($cfgValue) { return (int) $cfgValue === 0; },
                 true,
                 'string functions should not be overloaded',
                 'Set "<strong>mbstring.func_overload</strong>" to <strong>0</strong> in php.ini<a href="#phpini">*</a> to disable function overloading by the mbstring extension.'
@@ -323,7 +323,7 @@ final class BoltRequirements extends RequirementCollection
 
             $this->addPhpConfigRecommendation(
                 'intl.error_level',
-                create_function('$cfgValue', 'return (int) $cfgValue === 0;'),
+                function($cfgValue) { return (int) $cfgValue === 0; },
                 true,
                 'intl.error_level should be 0 in php.ini',
                 'Set "<strong>intl.error_level</strong>" to "<strong>0</strong>" in php.ini<a href="#phpini">*</a> to inhibit the messages when an error occurs in ICU functions.'
